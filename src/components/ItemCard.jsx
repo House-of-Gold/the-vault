b6a5777674a62964b4b697764cd1055b36486114
Ledjan Miçani <155323267+Choopin86@users.noreply.github.com>
2026-08-21 16:44:37 +0200

    Add item detail screen
    - mark as sold/undo sale
    - edit/delete gated by role

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router";
 import { supabase } from "../lib/supabaseClient";
 
 const ItemCard = ({ item }) => {
@@ -7,13 +8,13 @@ const ItemCard = ({ item }) => {
     : null;
 
   return (
-    <div className="item-card">
+    <Link className="item-card" to={`/item/${item.id}`}>
       {photoUrl ? <img src={photoUrl} alt={item.name} /> : null}
       <p>{item.name}</p>
       <p>{item.code}</p>
       <p>{item.price}</p>
       <p>{item.currency}</p>
-    </div>
+    </Link>
   );
 };
 
